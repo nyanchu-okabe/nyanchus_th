bfdd23847908922c7137e0b2e55b47ab875289ce
nyanchu-okabe <nyanchu.okabe@gmail.com>
2025-07-26 11:07:00 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+DJANGO_SUPERUSER_USERNAME=nyanchu DJANGO_SUPERUSER_EMAIL=nyanchu.okabe@gmail.com DJANGO_SUPERUSER_PASSWORD='OkabeDa_0709!' python manage.py createsuperuser --noinput
